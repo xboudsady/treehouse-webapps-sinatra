f7743ee89730c67787228f82f8b2e23011462835
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-01 21:38:38 -0700

    Added new form submission path to our main page. Add route /new for new new.erb path.

--- wiki/wiki.rb
+++ wiki/wiki.rb
@@ -2,6 +2,7 @@ require "sinatra"                   # Load Sinatra gem
 
 set :bind, "0.0.0.0"                # Allow remote access outside of localhost
 
+
 def page_content(title)             # Loads the content of a text file, return them as a string
     File.read("pages/#{title}.txt") # File is a sublass of the IO class, File inherits a class method named read. File.read() lets you read the contents of a file into a string, using only the file name.
 rescue Errno::ENOENT                # If the request file isn't found, the call File.read() may raise an Errno::ENOENT exception. "rescue" our program from being halted by the exception
@@ -14,8 +15,15 @@ def save_content(title, content)                    # First argument holds page 
     end
 end
 
-get "/" do                          # Root path
-    erb :welcome                    # render the welcome.erb from our /views folder
+
+# ===== ROUTES =====
+
+get "/" do                              # Root path
+    erb :welcome                        # render the welcome.erb from our /views folder
+end
+
+get "/new" do
+    erb :new
 end
 
 get "/:title" do                        # Get url path parameter
@@ -23,3 +31,4 @@ get "/:title" do                        # Get url path parameter
     @content = page_content(@title)     # Call the page_content(), passing in the .txt file to be read
     erb :show                           # Render an erb template
 end
+
